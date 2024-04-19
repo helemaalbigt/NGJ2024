f2cec5f53430df47eb800442f42e5a9acf73ee52
João Eiras Antunes <jantunes91@gmail.com>
2024-04-19 22:49:03 +0200

    Add Haptics Manager, Mine Manager and basic Mine object

---
+++ Assets/Scripts/HapticsManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus.Haptics;
+using UnityEditor;
+
+public class HapticsManager : MonoBehaviour
+{
+    private Dictionary<GUID, HapticClipPlayer> myPlayers;
+
+    public GUID PlayHapticClip(HapticClip aClip, bool aIsLooping = false, Controller aController = Controller.Both)
+    {
+        HapticClipPlayer player = new HapticClipPlayer(aClip);
+        player.isLooping = aIsLooping;
+        player.Play(aController);
+        GUID uid = GUID.Generate();
+        myPlayers.Add(uid, player);
+
+        return uid;
+    }
+
+    public void ChangeAmplitude(GUID aUID, float anAmplitude)
+    {
+        if (!myPlayers.ContainsKey(aUID))
+            return;
+
+        myPlayers[aUID].amplitude = Mathf.Clamp(anAmplitude, 0.0f, 1.0f);
+    }
+
+    public void ChangeFrequency(GUID aUID, float aFrequency)
+    {
+        if (!myPlayers.ContainsKey(aUID))
+            return;
+
+        myPlayers[aUID].frequencyShift = Mathf.Clamp(aFrequency, -1.0f, 1.0f);
+    }
+
+    public void Stop(GUID aUID)
+    {
+        if (!myPlayers.ContainsKey(aUID))
+            return;
+
+        myPlayers[aUID].Stop();
+    }
+
+    public void StopAll()
+    {
+        foreach (var player in myPlayers)
+        {
+            player.Value.Stop();
+        }
+
+        myPlayers.Clear();
+    }
+}
