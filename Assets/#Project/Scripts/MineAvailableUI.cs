c1f2507706eea75c833783f5d0a1a55d2d2d6e01
João Eiras Antunes <jantunes91@gmail.com>
2024-04-21 00:49:26 +0200

    Rotate mine in UI just because we can

--- Assets/#Project/Scripts/MineAvailableUI.cs
+++ Assets/#Project/Scripts/MineAvailableUI.cs
@@ -5,11 +5,14 @@ using UnityEngine.UI;
 
 public class MineAvailableUI : MonoBehaviour
 {
+    public float mineModelRotSpeed = 25.0f;
+    public GameObject mineModel;
     public Text text;
 
     // Update is called once per frame
     void Update()
     {
+        mineModel.transform.RotateAround(mineModel.transform.position, mineModel.transform.up, Time.deltaTime * mineModelRotSpeed);
         text.text = GameManager.Instance.GetAvailableMines().ToString() + "/" + GameManager.Instance._numberOfMinesPerRun.ToString();
     }
 }
