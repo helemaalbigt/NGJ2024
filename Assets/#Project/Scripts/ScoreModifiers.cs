33c7f26c07b7df1c1973de9a3bf7377b854bc788
VANBO\vanbo <van.bouwel.thomas@gmail.com>
2024-04-21 11:02:56 +0200

    scoremodifiers

---
+++ Assets/#Project/Scripts/ScoreModifiers.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ScoreModifiers {
+    public static int HitBonus = 15;
+    public static int HitPenality = 10;
+    public static int SelfHitPenality = 15;
+}
