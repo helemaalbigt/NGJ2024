5d786eb7691e67fa7bf2e22292e912202fc66626
VANBO\vanbo <van.bouwel.thomas@gmail.com>
2024-04-21 09:29:17 +0200

    keep head of detector flat

--- Assets/#Project/Scripts/HeadPositioner.cs
+++ Assets/#Project/Scripts/HeadPositioner.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class HeadPositioner : MonoBehaviour
 {
-
     void Update() {
-        var rot = transform.localRotation;
-        rot.eulerAngles = new Vector3(90f, 0, 0);
+        var targetRot = transform.parent.rotation;
+        if (transform.parent.rotation.eulerAngles.x > 10f && transform.parent.rotation.eulerAngles.x < 90f) {
+            targetRot = Quaternion.Euler(90f, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        } 
+        transform.rotation = Quaternion.Lerp( transform.rotation, targetRot, Time.unscaledDeltaTime * 5f);
     }
 }
